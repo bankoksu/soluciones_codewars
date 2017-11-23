5e830ccfc6e5fa5c280929e128a78a8f85af2ddf
bankoksu <seshomarualex@gmail.com>
2017-11-23 22:45:54 +0100

    ejercicios terminados y agregado terminacion _f

---
+++ ideas_palindrome_f.py
@@ -0,0 +1,52 @@
+
+def palindrome(num):            #23/11
+    try:
+        if num >= 0:
+    
+            num1 = (str(num))[::-1]
+            if int(num1) == num:
+                return True
+            else:
+                return False
+        else:
+            return "Not valid"
+    except:
+        return "Not valid"
+
+
+palindrome
+def palindrome(num):
+    
+    if num > 0:
+        num1 = (str(num))[::-1]
+        if int(num1) == num:
+            return True
+        else:
+            return False
+    
+    
+    else:
+        return "Not valid"
+
+def palindrome(num):
+    try:
+        if num > 0 and num == "":
+    
+            num1 = (str(num))[::-1]
+            if int(num1) == num:
+                return True
+            else:
+                return False
+                
+        except:
+            pass
+    
+        else:
+            return "Not valid"
+    
+
+
+
+
+
+print(palindrome("ACCDDCCA"))
